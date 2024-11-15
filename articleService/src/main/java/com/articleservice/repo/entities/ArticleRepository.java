99778c424cd50a337fb962df01e4a715f69bd988
ssr14 <ssr14@tpu.ru>
2024-11-15 16:20:17 +0700

    review services

--- articleService/src/main/java/com/articleservice/repo/entities/ArticleRepository.java
+++ articleService/src/main/java/com/articleservice/repo/entities/ArticleRepository.java
@@ -1,8 +1,9 @@
 package com.articleservice.repo.entities;
 
+import com.articleservice.domain.models.entities.ArticleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<com.storeservice.domain.models.entities.Article, Long> {
+public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
 }
